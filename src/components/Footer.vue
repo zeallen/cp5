b24f690cd1a732516fa7f133b9b0573599489ba7
Zachary Allen <zeallen3@byu.edu>
2019-04-14 16:00:14 -0600

    Added github footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer id="sticky">sticky footer content</footer>
+</template>
+
+<script>
+export default {
+  name: "ImageGallery"
+};
+</script>
+
+<style scoped>
+#sticky {
+  width: 100%;
+  height: 44px;
+  padding-top: 20px;
+  background: black;
+  color: white;
+  font-weight: bold;
+  font-size: 24px;
+  text-align: center;
+  position: fixed; /*Here's what sticks it*/
+  bottom: 0; /*to the bottom of the window*/
+  left: 0; /*and to the left of the window.*/
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,17 @@
 <template>
-<div class="home">
-  <image-gallery :photos="photos" />
-</div>
+  <div class="home">
+    <image-gallery :photos="photos"/>
+    <footer id="sticky">  <a href="https://github.com/BYU-CS-260-Winter-2019/lab-5-zeallen">Github</a></footer>
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import ImageGallery from '@/components/ImageGallery.vue'
+import ImageGallery from "@/components/ImageGallery.vue";
+import Footer from "@/components/Footer.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
     ImageGallery
   },
@@ -20,6 +22,22 @@ export default {
   },
   async created() {
     await this.$store.dispatch("getAllPhotos");
-  },
-}
+  }
+};
 </script>
+
+<style scoped>
+#sticky {
+  width: 100%;
+  height: 44px;
+  padding-top: 10px;
+  background: black;
+  color: white;
+  font-weight: bold;
+  font-size: 24px;
+  text-align: center;
+  position: fixed; /*Here's what sticks it*/
+  bottom: 0; /*to the bottom of the window*/
+  left: 0; /*and to the left of the window.*/
+}
+</style>
